6fa40c1b8815d3dfe6cb3b06d37702d888c3b2f6
tl1l1l1s <s2oo1og24@gmail.com>
2025-09-01 21:40:12 +0900

    feat: swagger, exception setting

---
+++ src/main/java/somsomcore/zipcheck/global/apiPayload/exception/GeneralException.java
@@ -0,0 +1,22 @@
+package somsomcore.zipcheck.global.apiPayload.exception;
+
+import somsomcore.zipcheck.global.apiPayload.code.BaseErrorCode;
+import somsomcore.zipcheck.global.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+
+}
